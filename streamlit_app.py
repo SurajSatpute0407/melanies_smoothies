1730eb77533ab42c5d6e6273ffdb64c5a152959e
Suraj Satpute <112246730+SurajSatpute0407@users.noreply.github.com>
2025-06-19 13:23:13 +0530

    Update streamlit_app.py
    Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ name = st.text_input('Your name')
 
 cnx= st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name')).select(col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'), col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
